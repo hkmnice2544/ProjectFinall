df9ba039240d343c776ffbdf6f2026c6de2b5790
hkmnice2544 <Hatsaya13759@gmail.com>
2023-09-14 01:36:59 +0700

    Commit  ADD Get Review

--- src/main/java/org/itsci/informrepair/controller/ReportRepairController.java
+++ src/main/java/org/itsci/informrepair/controller/ReportRepairController.java
@@ -1,5 +1,6 @@
 package org.itsci.informrepair.controller;
 
+import org.itsci.informrepair.model.Equipment;
 import org.itsci.informrepair.model.InformRepair;
 import org.itsci.informrepair.model.Reportrepair;
 import org.itsci.informrepair.service.InformRepairService;
@@ -7,10 +8,7 @@ import org.itsci.informrepair.service.ReportrepairService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -45,6 +43,17 @@ public class ReportRepairController {
             return  new ResponseEntity(reportrepairs,HttpStatus.OK);
         }catch (Exception e){
             return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/get/{report_id}")
+    public ResponseEntity<?> getReportrepairById(@PathVariable Integer report_id) {
+        try {
+            Reportrepair reportrepairs = reportrepairService.getReportrepairById(report_id);
+            return ResponseEntity.ok(reportrepairs);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.notFound().build();
         }
     }
 
