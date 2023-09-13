29bbb3cbe43e96cdf2745085d52cf2337f8d852e
hkmnice2544 <Hatsaya13759@gmail.com>
2023-09-14 00:35:33 +0700

    Commit  ADD List Review

--- src/main/java/org/itsci/informrepair/controller/ReviewController.java
+++ src/main/java/org/itsci/informrepair/controller/ReviewController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -34,6 +35,17 @@ public class ReviewController {
         }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/list")
+    public ResponseEntity<List<Review>> listReview() {
+        try {
+            List<Review> reviews = reviewService.getAllReviews();
+            return new ResponseEntity<>(reviews, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
